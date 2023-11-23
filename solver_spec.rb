b9a1348bdc546724e2fb1d3a9659d554d9b55102
lily4178993 <nellytelli@gmail.com>
2023-11-23 19:07:59 +0000

    feat: add unit tests for fizzbuzz method

--- solver_spec.rb
+++ solver_spec.rb
@@ -25,7 +25,25 @@ describe Solver do
     end
 
     describe '#fizzbuzz' do
-      # test
+      it 'is a valid method' do
+        expect(solver.respond_to?(:fizzbuzz)).to be(true)
+      end
+
+      it 'returns when N is divisible by only 3 should return "fizz"' do
+        expect(solver.fizzbuzz(2)).to eq('fizz')
+      end
+
+      it 'returns when N is divisible by only 5 should return "buzz"' do
+        expect(solver.fizzbuzz(99)).to eq('buzz')
+      end
+
+      it 'returns when N is divisible by by both 3 and 5 should return "fizzbuzz"' do
+        expect(solver.fizzbuzz(89)).to eq('fizzbuzz')
+      end
+
+      it 'returns when N is not divisible by either 3 or 5 should return the number itself' do
+        expect(solver.fizzbuzz(7)).to eq('7')
+      end
     end
   end
 end
