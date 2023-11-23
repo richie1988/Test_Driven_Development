917bec7a2c0bc9a08174adf8c7887dfe5098abc8
lily4178993 <nellytelli@gmail.com>
2023-11-23 18:26:19 +0000

    feat: add tests for reverse method

--- solver_spec.rb
+++ solver_spec.rb
@@ -1,4 +1,31 @@
+# solver_spec.rb
 require 'rspec'
 require_relative 'solver'
 
-# tests
+describe Solver do
+  it 'is a class' do
+    expect(described_class).to be_a(Class)
+  end
+
+  context 'when initialized' do
+    let(:solver) { Solver.new }
+
+    describe '#factorial' do
+      # test
+    end
+
+    describe '#reverse' do
+      it 'is a valid method' do
+        expect(solver.respond_to?(:reverse)).to be(true)
+      end
+
+      it 'returns the reversed string' do
+        expect(solver.reverse('hello')).to eq('olleh')
+      end
+    end
+
+    describe '#fizzbuzz' do
+      # test
+    end
+  end
+end
